7c7a25dabd30d15f23b51e64d59a552e23d29fee
Otus9051 <stevewholikesthings@gmail.com>
2022-07-02 19:57:36 +0530

    Java: Exception Handling

---
+++ Java/ExceptionHandling/except1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class except1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, res;
+        System.out.println("Enter two numbers");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        try {
+            res = n1 / n2;
+            System.out.println("Result = " + res);
+        } catch (InputMismatchException e) {
+            System.out.println("System Message = " + e.getMessage());
+            System.out.println("User Message = Input value is not Numeric");
+        } catch (ArithmeticException e) {
+            System.out.println("System Message = " + e.getMessage());
+            System.out.println("User Message = You cannot divide a number by zero");
+        }
+        finally {
+            System.out.println("Thank you for using this program");
+        }
+    }
+}
