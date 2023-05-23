020635ae1cd35df718716c0a65b4f5fcd60c6018
Otus9051 <stevewholikesthings@gmail.com>
2023-05-23 10:26:53 +0530

    Forgot two files

--- Java/String/StrCharAt.java
+++ Java/String/StrCharAt.java
@@ -5,8 +5,9 @@ class StrCharAt
     {
         String str;
         System.out.println("Enter your sentence");
-        Scanner sc = new Scanner(System.in);
-        str = sc.next();
+        try (Scanner sc = new Scanner(System.in)) {
+            str = sc.next();
+        }
         System.out.println("The 1st Character of the sentence is "+str.charAt(0));
     }
 }
--- Java/String/StrConcat.java
+++ Java/String/StrConcat.java
@@ -5,10 +5,11 @@ class StrConcat
     {
         String firstName, lastName, finalName;
         System.out.println("Enter your first name: ");
-        Scanner sc = new Scanner(System.in);
-        firstName = sc.next();
-        System.out.println("Enter your last name: ");
-        lastName = sc.next();
+        try (Scanner sc = new Scanner(System.in)) {
+            firstName = sc.next();
+            System.out.println("Enter your last name: ");
+            lastName = sc.next();
+        }
         finalName = firstName.concat(" ");
         finalName = finalName.concat(lastName);
         System.out.println("Your full name is "+finalName);
