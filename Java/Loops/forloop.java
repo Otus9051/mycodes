678513ab9486ea3455f3899f1d3df3c1aaf317dc
Otus9051 <stevewholikesthings@gmail.com>
2023-08-07 19:15:47 +0530

    Java: for loop demonstration

---
+++ Java/Loops/forloop.java
@@ -0,0 +1,10 @@
+class forloop {
+    public static void main(String[] args) {
+        int i = 1;
+        for (; i <= 10; i++) {
+            if (i == 5)
+                continue;
+            System.out.println(i + " ");
+        }
+    }
+}
